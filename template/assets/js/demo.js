ad80e2080cdfc1218e8fbba3f94dffeb424dddeb
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-05-31 14:10:25 -0400

    _

--- template/assets/js/demo.js
+++ template/assets/js/demo.js
@@ -4,12 +4,7 @@ demo = {
     initContactUsMap: function(){
 
         var myLatlng = new google.maps.LatLng(38.846454, -77.140064);
-        var mapOptions = {
-          zoom: 14,
-          center: myLatlng,
-          styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}],
-          scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-        }
+        var mapOptions = { zoom: 11, center: myLatlng, styles: [{ featureType: "administrative", elementType: "geometry", stylers: [{ color: "#808080" }, { visibility: "off" }, { invert_lightness: true }] }, { featureType: "water", stylers: [{ saturation: -100 }, { lightness: 51 }] }, { featureType: "landscape", elementType: "geometry", stylers: [{ color: "#808080" }] }, { featureType: "poi", stylers: [{ visibility: "off" }] }, { featureType: "road", elementType: "geometry", stylers: [{ lightness: 100 }] }, { elementType: "labels.icon", stylers: [{ saturation: -100 }, { gamma: 0.71 }] }, { featureType: "transit", stylers: [{ saturation: -100 }] }, { elementType: "labels.text", stylers: [{ saturation: -100 }, { lightness: 8 }] }], scrollwheel: false }; //we disable de scroll over the map, it is a really annoing when you scroll through page
 
         var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
 
